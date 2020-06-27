4949520db03c7a45174b32eeb90df7e5da64bc3f
Hanif Fakira <fakira.work@gmail.com>
2020-06-27 10:53:36 +0100

    refactor test: make all test symetric easy to read and consistent

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,20 +1,18 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class YatzyTest {
 
   @Test
   public void chance_scores_sum_of_all_dice() {
-    int expected = 15;
-    int actual = Yatzy.chance(2,3,4,5,1);
-    assertEquals(expected, actual);
+    assertEquals(15, Yatzy.chance(2,3,4,5,1));
     assertEquals(16, Yatzy.chance(3,3,4,5,1));
   }
 
   @Test public void yatzy_scores_50() {
-    int expected = 50;
-    int actual = Yatzy.yatzy(4,4,4,4,4);
-    assertEquals(expected, actual);
+    assertEquals(50, Yatzy.yatzy(4,4,4,4,4));
     assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
     assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
   }
